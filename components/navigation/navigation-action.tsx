c29f410745576baa3ffa40912544cd543b143898
JamesAskelson <jamesaskelson630@gmail.com>
2024-04-22 21:31:57 -0500

    finished-navi-sidebar-plus-fixes-to-light-dark-and-ui

---
+++ components/navigation/navigation-action.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+import { Plus } from "lucide-react";
+import { ActionTooltip } from "../ui/action-tooltip";
+import InitialModal from "../modals/initial-modal";
+
+export const NavigationAction = () => {
+    const onClick = () => {
+        <InitialModal />
+    }
+
+    return (
+        <div>
+            <ActionTooltip label='Add a server' side='right' align='center'>
+                <button onClick={onClick} className='group flex items-center'>
+                    <div className='flex mx-3 h-[48px] w-[48px] rounded-[24px] group-hover:rounded-[16px] transition-all overflow-hidden items-center justify-center bg-background dark:bg-neutral-800 group-hover:bg-emerald-500'>
+                        <Plus
+                        className='group-hover:text-white transition text-emerald-500'
+                        />
+                    </div>
+                </button>
+            </ActionTooltip>
+        </div>
+    )
+}
---
+++ components/navigation/navigation-sidebar.tsx
@@ -0,0 +1,77 @@
+import { currentProfile } from "@/lib/current-profle";
+import { db } from "@/lib/db";
+import Image from "next/image";
+import { redirect } from "next/navigation";
+import { NavigationAction } from "./navigation-action";
+import { MoonStar } from "lucide-react";
+import { ActionTooltip } from "../ui/action-tooltip";
+import { Separator } from "@/components/ui/separator"
+import { ScrollArea } from "../ui/scroll-area";
+import { NavigationItem } from "./navigation-item";
+import { UserButton } from "@clerk/nextjs";
+import { ModeToggle } from "../ui/theme-button";
+export const NavigationSideBar = async () => {
+    const profile = await currentProfile()
+
+    if(!profile){
+        return redirect('/')
+    }
+
+    const servers = await db.server.findMany({
+        where: {
+            members: {
+                some: {
+                    profileId: profile.id
+                }
+            }
+        }
+    })
+
+
+    return (
+        <div className="bg-zinc-300 dark:bg-zinc-700">
+            <div className="flex w-full h-[72px]">
+                <ActionTooltip label='Direct Messages' side='right' align='center'>
+                    <button className='group flex items-center '>
+                        <div className='flex mx-3 h-[48px] w-[48px] rounded-[24px] group-hover:rounded-[16px] transition-all overflow-hidden items-center justify-center bg-background dark:bg-neutral-800 group-hover:bg-emerald-500'>
+                            <MoonStar
+                            className='group-hover:text-white transition text-emerald-500'
+                            />
+                        </div>
+                    </button>
+                </ActionTooltip>
+            </div>
+
+            <Separator className="h-[2px] bg-zinc-400  dark:bg-zinc-500  w-16 mx-auto" />
+
+                <ScrollArea className='flex-1 w-full space-y-4'>
+                    <div className="space-y-4">
+                        {servers.map((server) => (
+                            <NavigationItem id={server.id} name={server.name} imageUrl={server.imageUrl}/>
+                        ))}
+                    </div>
+
+                    <Separator className="h-[2px] bg-zinc-400 dark:bg-zinc-500  w-16 mx-auto mt-4" />
+
+                    <div className="space-y-4 flex-col items-center h-full text-primary w-full py-3">
+                        <NavigationAction />
+                    </div>
+                </ScrollArea>
+
+
+
+            <div className='left-1 absolute bottom-0 pb-3 mt-auto flex items-center flex-col gap-y-4 bg-zinc-300 dark:bg-zinc-700'>
+            <Separator className=" h-[2px] bg-zinc-200 dark:bg-zinc-500 w-16" />
+                <ModeToggle />
+                <UserButton
+                    afterSignOutUrl='/'
+                    appearance={{
+                        elements: {
+                            avatarBox: 'h-[48px] w-[48px]'
+                        }
+                    }}
+                />
+            </div>
+        </div>
+     );
+}
