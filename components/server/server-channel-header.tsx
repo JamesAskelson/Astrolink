b46d2aae0689b3b9f349f6475e44cc4a5fe6cd2f
JamesAskelson <jamesaskelson630@gmail.com>
2024-04-25 20:35:32 -0500

    finished-channel-sidebar-ui

---
+++ components/server/server-channel-header.tsx
@@ -0,0 +1,69 @@
+'use client';
+import { ServerWithMembersWithProfiles } from "@/type";
+import { MemberRole } from "@prisma/client";
+import { DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger, } from "../ui/dropdown-menu";
+import { ChevronDown, UserPlus, Wrench, ListPlus, Trash2, DoorOpen } from "lucide-react";
+
+
+interface ServerHeaderProps {
+    server: ServerWithMembersWithProfiles;
+    role?: MemberRole
+}
+
+const ServerChannelHeader = ({server, role}: ServerHeaderProps) => {
+
+    const isAdmin = role === MemberRole.ADMIN
+    const isModerator = role === MemberRole.MODERATOR
+
+    return (
+        <div>
+            <DropdownMenu>
+                <DropdownMenuTrigger className="focus:outline-none" asChild>
+                    <button className="w-full font-semibold px-6 flex items-center h-12 border-neutral-300 dark:border-neutral-900 border-b-4 hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition">
+                        {server.name}
+                    <ChevronDown className='h-5 w-5 ml-auto' />
+                    </button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent className='w-56 text-xs font-medium text-black dark:text-neutral-400 space-y-[2px]'>
+                    {isModerator || isAdmin &&
+                    (<DropdownMenuItem className='text-indigo-300 focus:text-indigo-600 cursor-pointer'>
+                        Invite People
+                    <UserPlus className='h-5 w-5 ml-auto' />
+                    </DropdownMenuItem>)
+                    }
+                    {isAdmin &&
+                    (<DropdownMenuItem className='text-gray-400 focus:text-gray-600 dr:focus:text-gray-800 cursor-pointer'>
+                        Server Settings
+                        <Wrench className='h-5 w-5 ml-auto' />
+                    </DropdownMenuItem>)
+                    }
+                    {isModerator || isAdmin &&
+                    (<DropdownMenuItem className='text-gray-400 focus:text-gray-600 dr:focus:text-gray-800 cursor-pointer'>
+                        Create Channel
+                        <ListPlus className='h-5 w-5 ml-auto' />
+                    </DropdownMenuItem>)
+                    }
+                    {isAdmin &&
+                    (<DropdownMenuItem className='text-red-500 focus:text-red-600 cursor-pointer'>
+                        Delete Server
+                        <Trash2 className='h-5 w-5 ml-auto' />
+                    </DropdownMenuItem>)
+                    }
+                    {!isAdmin &&
+                    (<DropdownMenuItem className='text-red-500 focus:text-red-600 cursor-pointer'>
+                        Leave Server
+                        <DoorOpen className='h-5 w-5 ml-auto' />
+                    </DropdownMenuItem>)
+                    }
+                </DropdownMenuContent>
+            </DropdownMenu>
+        </div>
+     );
+}
+
+export default ServerChannelHeader;
