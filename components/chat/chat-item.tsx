4e48435ec0fe72522d37c356979882b5c899cd91
JamesAskelson <jamesaskelson630@gmail.com>
2024-08-09 22:10:51 -0500

    started-edit

--- components/chat/chat-item.tsx
+++ components/chat/chat-item.tsx
@@ -1,9 +1,11 @@
 import { Member, MemberRole, Profile } from "@prisma/client";
 import UserAvatar from "../profile/user-profile";
-import { FileIcon, ShieldAlert, ShieldX } from "lucide-react";
+import { Edit, FileIcon, ShieldAlert, ShieldX } from "lucide-react";
 import { ActionTooltip } from "../ui/action-tooltip";
 import { isAppPageRouteDefinition } from "next/dist/server/future/route-definitions/app-page-route-definition";
 import Image from "next/image";
+import { useState } from "react";
+import { cn } from "@/lib/utils";
 
 interface ChatItemProps {
     id: string;
@@ -28,6 +30,10 @@ const roleIconMap = {
 }
 
 const ChatItem = ({id, text, member, timestamp, fileUrl, deleted, currentMember, isUpdated, socketQuery, socketUrl}: ChatItemProps) => {
+    const [editing, setEditing] = useState(false);
+    const [deleting, setDeleting] = useState(false)
+
+
     const fileType = fileUrl?.split(".").pop()
 
 
@@ -55,11 +61,31 @@ const ChatItem = ({id, text, member, timestamp, fileUrl, deleted, currentMember,
                     <div className="text-xs text-slate-400">
                         {timestamp}
                     </div>
+                    {canDelete && (
+                    <div className="group-hover:flex items-center gap-x-2 group-hover:bg-white dark:bg-zinc-800 border rounded-sm">
+                        {canEdit && (
+                            <ActionTooltip label='Edit'>
+                                <Edit
+                                onClick={() => setEditing(true)}
+                                className='cursor-pointer ml-auto w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:hover:text-zinc-300 transition'/>
+                            </ActionTooltip>
+                        )}
+                    </div>
+                )}
                 </div>
                 <div className="flex flex-col">
-                    <div>
-                        {text}
-                    </div>
+                    {!fileUrl && !editing && (
+                            <p className={cn(
+                                "text-sm text-zinc-600 dark:text-zinc-300", deleted && 'italic text-zinc-500 dark:text-zinc-400 text-xs mt-1'
+                            )}>
+                                {text}
+                                {isUpdated && !deleted && (
+                                    <span className='text-[10px] mx-2 text-zinc-500 dark:text-zinc-400'>
+                                        (edited)
+                                    </span>
+                                )}
+                            </p>
+                        )}
                     <div>
                         {isImage &&
                         <a
